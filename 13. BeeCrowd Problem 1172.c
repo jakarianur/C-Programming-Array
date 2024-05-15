c153e3b9ca62f846898d5a7f40bfcdd704730690
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-15 15:15:53 +0600

    Create 13. BeeCrowd Problem 1172.c

---
+++ 13. BeeCrowd Problem 1172.c
@@ -0,0 +1,12 @@
+// https://www.beecrowd.com.br/judge/en/problems/view/1172
+
+#include <stdio.h> 
+int main() { 
+   int i,n[10]; 
+    for(i=0; i<10; i++) { 
+        scanf("%d",&n[i]); 
+        if(n[i]<=0) 
+            n[i] = 1; } 
+    for(i=0; i<10; i++) 
+        printf("X[%d] = %d\n",i,n[i]); 
+    return 0; }
