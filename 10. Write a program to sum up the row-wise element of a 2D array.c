fd3c7cdbaae10dc56c5530b77f4740c19a700c73
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-15 15:12:34 +0600

    Create 10. Write a program to sum up the row-wise element of a 2D array.c

---
+++ 10. Write a program to sum up the row-wise element of a 2D array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int main() {  
+int array[3][3],i,j,sum=0;  
+printf("Enter 3 by 3 array element: \n"); 
+for (i=0;i<3;i++){ 
+    for (j=0;j<3;j++){ 
+    scanf("%d", &array[i][j]);}} 
+    printf("\n"); 
+for (i=0;i<3;i++){ 
+    for(j=0;j<3;j++){ 
+        printf("%d\t", array[i][j]); 
+        sum=sum+array[i][j];} 
+        printf("Sum = %d\n", sum); 
+        sum=0; } 
+return 0; } 
