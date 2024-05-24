e89dff80470d9474d416301c62048e3f7cc2a8da
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 22:44:01 +0600

    Update 05. Write a program that searches for a specific element from an array.c

--- 05. Write a program that searches for a specific element from an array.c
+++ 05. Write a program that searches for a specific element from an array.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-int main(){
-    int arr[100], i, n, search, count=0;
-    printf("Enter any array aize: ");
-    scanf("%d", &n);
-    printf("Enter %d Array Element: ", n);
-    for(i=0;i<n;i++){
-        scanf("%d", &arr[i]);
+int main()
+{
+int arr[100], i, n, search, count=0;
+printf("Enter any array aize: ");
+scanf("%d",&n);
+printf("Enter %d Array Element: ", n);
+for(i=0;i<n;i++)
+{
+    scanf("%d", &arr[i]);
+}
+printf("Enter the element you want to search for: ");
+scanf("%d", &search);
+for(i=0;i<n;i++)
+{
+    if (arr[i]==search)
+    {
+        count=1;
+        break;
     }
-    printf("Enter the element you want to search for: ");
-    scanf("%d", &search);
-        for(i=0;i<n;i++){
-            if (arr[i]==search){
-                count=1;
-                break;
-            }
-        }
-        if (count==1){
-            printf("Element Found\n");
-        }
+}
+    if (count==1)
+    {
+        printf("Element Found\n");
+    }
         else printf("Element is not Found\n");
     return 0;
 }
