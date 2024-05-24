7c00f635cfff5536a989562a332ba7a20c1a47e0
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 22:58:17 +0600

    Update 07. Write a program to insert an element in a 1D array.c

--- 07. Write a program to insert an element in a 1D array.c
+++ 07. Write a program to insert an element in a 1D array.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-int main(){
-    int arr[100],i,n,position, value;
+int main()
+{
+    int arr[100],i,n,position,value;
     printf("Enter any array aize: ");
     scanf("%d", &n);
     printf("Enter %d Array Element: ", n);
-    for(i=0;i<n;i++){
-        scanf("%d", &arr[i]);
-    }
+    for(i=0;i<n;i++)
+{
+    scanf("%d", &arr[i]);
+}
     printf("Enter which position you want to insert: ");
     scanf("%d", &position);
     printf("Enter the value you want to insert: ");
     scanf("%d", &value);
-        for(i=n;i>=position;i--){
+    for(i=n;i>=position;i--)
+    {
             arr[i]>arr[i-1];
-        }
+    }
            arr[position-1]=value; 
            printf("Insertion: ");
-            for(i=0;i<n+1;i++){
-               printf("%d ", arr[i]); 
-        }
+    for(i=0;i<n+1;i++)
+    {
+        printf("%d ", arr[i]); 
+    }
     return 0;
 }
