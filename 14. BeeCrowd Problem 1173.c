5ad0d63977a07bcea8e73cc1eab8d31d02e534af
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:12:33 +0600

    Update 14. BeeCrowd Problem 1173.c

--- 14. BeeCrowd Problem 1173.c
+++ 14. BeeCrowd Problem 1173.c
@@ -1,14 +1,21 @@
 // https://www.beecrowd.com.br/judge/en/problems/view/1173 
 
 #include <stdio.h> 
-int main() { 
+int main()
+{ 
     int n[10],x,i; 
     scanf("%d",&x); 
-    for(i=0; i<10; i++) { 
+    for(i=0; i<10; i++)
+    { 
         if(i==0) 
-            n[i] = x; 
-        else 
-            n[i] = n[i-1]*2; } 
-    for(i=0; i<10; i++) 
+        n[i] = x; 
+    }
+    else 
+        n[i] = n[i-1]*2;
+    } 
+    for(i=0; i<10; i++)
+    {
         printf("N[%d] = %d\n",i,n[i]); 
-    return 0; } 
+    }
+    return 0;
+} 
