6082e489455074ce98a4570efc377acd38b4c7ce
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:08:41 +0600

    Update 12. Arithmetic Operation on 2D matrix (Addition, Subtruction, Multiplication).c

--- 12. Arithmetic Operation on 2D matrix (Addition, Subtruction, Multiplication).c
+++ 12. Arithmetic Operation on 2D matrix (Addition, Subtruction, Multiplication).c
@@ -6,35 +6,38 @@ int main()
     int Add[2][2], Sub[2][2], Mul[2][2];
     int i,j,k;
     printf("Matrix Addition: \n");
-    for (i=0; i<2; i++) {
+    for (i=0; i<2; i++)
+    {
         for (j=0;j<2; j++) 
         {
         Add[i][j] = A[i][j] + B[i][j];
         printf("%d ", Add[i][j]);
         }
         printf("\n");
-        }
+    }
         printf("Matrix Subtraction: \n");
-    for (i=0; i<2; i++){
+    for (i=0; i<2; i++)
+    {
         for (j=0;j<2; j++)
         {
         Sub[i][j] = A[i][j] - B[i][j];
         printf("%d ", Sub[i][j]);
         }
         printf("\n");
-        }
+    }
         printf("Matrix Multiplication: \n");
-    for (i=0; i<2; i++){
+    for (i=0; i<2; i++)
+    {
         for (j=0;j<2; j++)
         {
             Mul[i][j] = 0;
-            for (k=0; k<2; k++)
+        for (k=0; k<2; k++)
             {
         Mul[i][j] += A[i][k] * B[k][j];
             }
         printf("%d ", Mul[i][j]);
         }
         printf("\n");
-        }
+    }
         return 0;
 }
