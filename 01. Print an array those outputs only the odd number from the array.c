46a6ba6ab5ece5af2f344ba5c3571646c2a02cc0
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 22:28:41 +0600

    Update 01. Print an array those outputs only the odd number from the array.c

--- 01. Print an array those outputs only the odd number from the array.c
+++ 01. Print an array those outputs only the odd number from the array.c
@@ -1,14 +1,19 @@
 #include <stdio.h> 
-int main(){ 
-int a, b; 
-printf("Enter your any array size: "); 
-scanf("%d", &a); 
-int nur[a]; 
-printf("Enter %d Numbers: ", a); 
-for(b=0;b<a;b++){ 
-scanf("%d",&nur[b]);} 
+int main()
+{ 
+int i, n; 
+printf("Enter any array size: "); 
+scanf("%d", &n); 
+int arr[n]; 
+printf("Enter %d Numbers: ", n); 
+for(i=0;i<n;i++)
+{ 
+scanf("%d",&arr[i]);
+} 
 printf("Odd Numbers in Array are: "); 
-for(b=0;b<a;b++){ 
-if(nur[b]%2!=0) 
-printf("%d ",nur[b]);} 
+for(i=0;i<n;i++)
+{ 
+if(arr[i]%2!=0) 
+printf("%d ",arr[i]);
+} 
 return 0;} 
