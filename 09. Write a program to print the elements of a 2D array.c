c84ce47ce40896a20f64bf7ae460c232110d648f
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:01:58 +0600

    Update 09. Write a program to print the elements of a 2D array.c

--- 09. Write a program to print the elements of a 2D array.c
+++ 09. Write a program to print the elements of a 2D array.c
@@ -1,12 +1,22 @@
 #include<stdio.h>  
-int main() {  
+int main()
+{  
 int array[3][3],i,j;  
 printf("Enter 3 by 3 array element: \n"); 
-for (i=0;i<3;i++){ 
-    for (j=0;j<3;j++){ 
-    scanf("%d", &array[i][j]);}} 
-for (i=0;i<3;i++){ 
-    for(j=0;j<3;j++){ 
-        printf("%d\t", array[i][j]);} 
-        printf("\n");} 
-return 0; }
+for (i=0;i<3;i++)
+{ 
+    for (j=0;j<3;j++)
+    { 
+    scanf("%d", &array[i][j]);
+    }
+} 
+for (i=0;i<3;i++)
+{ 
+    for(j=0;j<3;j++)
+    { 
+    printf("%d\t", array[i][j]);
+    } 
+    printf("\n");
+} 
+return 0;
+}
