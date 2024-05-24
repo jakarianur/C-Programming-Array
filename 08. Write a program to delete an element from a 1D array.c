60882c525c6972a50d5cc62ac810ee3a0315faaa
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:00:00 +0600

    Update 08. Write a program to delete an element from a 1D array.c

--- 08. Write a program to delete an element from a 1D array.c
+++ 08. Write a program to delete an element from a 1D array.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int arr[100],i,n,position;
     printf("Enter any array aize: ");
     scanf("%d", &n);
     printf("Enter %d Array Element: ", n);
-    for(i=0;i<n;i++){
-        scanf("%d", &arr[i]);
-    }
+for(i=0;i<n;i++)
+{
+    scanf("%d", &arr[i]);
+}
     printf("Enter which position you want to delete: ");
     scanf("%d", &position);
-        for(i=position-1;i<=n-2;i++){
-            arr[i]=arr[i+1];
-        }
-            arr[n-1]=0;
-           printf("Deletion: ");
-            for(i=0;i<n-1;i++){
-               printf("%2d ", arr[i]); 
-        }
+for(i=position-1;i<=n-2;i++)
+{
+    arr[i]=arr[i+1];
+}
+        arr[n-1]=0;
+        printf("Deletion: ");
+for(i=0;i<n-1;i++)
+{
+        printf("%2d ", arr[i]); 
+}
     return 0;
 }
